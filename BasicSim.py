3743502152f87639dbd6f4ccae3e82a889d0b98a
Andrew Hilton <anhilton@silabs.com>
2019-11-09 16:52:16 -0500

    Adding a simple Duel simulation:   input two stat blocks, and they take turns smacking each other until one is dead   also figured out a neat trick to do conditional prints, gonna use that more often

--- BasicSim.py
+++ BasicSim.py
@@ -1,6 +1,7 @@
 """
 functions to define basic simulation trials
 """
+from collections import deque
 
 from DndDice import *
 
@@ -55,3 +56,36 @@ def runTrialAvgNumHits(n, targetHp, targetAc, attackMod, damageDice, damageMod):
     totalAttempts += attempts
     totalHits += success
   return (totalAttempts/n,totalHits/n)
+
+
+def simulatedDuel(charA, charB, verbose=True):
+
+  fightLog = print if verbose else lambda x : None
+  # set their attacks to the default
+  charA.reset()
+  charB.reset()
+
+  combatants = deque([charA, charB])
+  winner = None
+  loser = None
+
+  roundNum = 0
+  Done = False
+  while not Done:
+    current = combatants[0]
+    opponent = combatants[1]
+    fightLog(f"{current.name} attacks {opponent.name}")
+    hit = current.makeAttack(opponent)
+    if hit:
+      fightLog("Hit!")
+      if opponent.isDead():
+        Done = True
+        fightLog("Fatal Blow")
+        winner = current
+        loser = opponent
+    else:
+      fightLog("Miss!")
+    combatants.rotate()
+    roundNum += 1
+  fightLog(f"{winner.name} is victorious")
+  return winner
